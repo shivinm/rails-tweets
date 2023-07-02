4a65f5c3e6dd7813c95d063651f8755fe3078994
Ubuntu <ubuntu@ip-172-31-6-242.us-west-1.compute.internal>
2023-07-02 22:16:22 +0000

    add email validation to model

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
   def index
-    flash[:alert] = "This is an alert!"
-    flash[:notice] = "This is a notice!"
+    flash.now[:alert] = "This is an alert!"
+    flash.now[:notice] = "This is a notice!"
   end
 end
