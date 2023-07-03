9f9ecded57f78507e517107136bd36d6445322f1
Shivin Misra <shivinmisra@gmail.com>
2023-07-03 18:41:03 +0000

    add feature to login user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,26 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user.present? && user.authenticate(params[:password])
+      if session[:user_id].present?
+        redirect_to root_path, notice: "User is already logged in!"
+      else
+        session[:user_id] = user.id
+        redirect_to root_path, notice: "Logged in successfully!"
+      end
+    else
+      flash[:alert] = "Invalid email or password"
+      render :new
+    end
+  end
+
   def destroy
     session[:user_id] = nil
     redirect_to root_path, notice: "Logged out!"
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   get "/about", to: "about#index", as: :about
-  get "/sign_up", to: "registrations#new"
-  post "/sign_up", to: "registrations#create"
+
+  get "/sign_up", to: "registrations#new"   # gets the sign-up form
+  post "/sign_up", to: "registrations#create" # creates a user
+
+  get "/sign_in", to: "sessions#new"        # gets the login form
+  post "/sign_in", to: "sessions#create"    # logs in user
+
   delete "/logout", to: "sessions#destroy"
+
   get "/", to: "main#index", as: :root
 end
