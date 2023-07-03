e46be990ead6b9cce0896b854aa482fb99ece118
Shivin Misra <shivinmisra@gmail.com>
2023-07-03 01:45:50 +0000

    add forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/about-us", to: "about#index", as: :about
+  get "/sign-up", to: "registrations#new"
+  post "/users", to: "registrations#create"
   get "/", to: "main#index", as: :root
 end
